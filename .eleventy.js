bf2e047de54b63f5529a65a450dbc02a7ae0b815
Parth <parth.shiralkar@gmail.com>
2024-04-17 11:08:00 -0700

    Pass fontfile

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/img')
     eleventyConfig.addPassthroughCopy('src/js')
     eleventyConfig.addPassthroughCopy('src/lib')
+    eleventyConfig.addPassthroughCopy('src/fontfiles')
 
     return {
         passthroughFileCopy: true,
