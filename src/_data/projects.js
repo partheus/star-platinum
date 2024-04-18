0f0317423ce8aee169de23f4a9f57edd19464e80
Parth <parth.shiralkar@gmail.com>
2024-04-18 14:46:17 -0700

    Add dummy projects

--- src/_data/projects.js
+++ src/_data/projects.js
@@ -4,7 +4,7 @@ module.exports = [
         id: 1,
         title: "Project One",
         subtitle: "An amazing project",
-        image: "https://placehold.co/600x400/000000/FFF",
+        image: "https://placehold.co/500x400/000000/FFF",
         tech: ["HTML", "CSS", "JavaScript"],
         link: "/projects/project-one",
         liveSite: "https://example.com/project-one"
@@ -13,10 +13,38 @@ module.exports = [
         id: 2,
         title: "Project Two",
         subtitle: "Another remarkable project",
-        image: "https://placehold.co/600x400/000000/FFF",
+        image: "https://placehold.co/500x400/000000/FFF",
         tech: ["Node.js", "Express", "MongoDB"],
         link: "/projects/project-two",
         liveSite: "https://example.com/project-two"
+    },
+    {
+        id: 3,
+        title: "Project Three",
+        subtitle: "A third project",
+        image: "https://placehold.co/500x400/000000/FFF",
+        tech: ["React", "Redux", "Firebase"],
+        link: "/projects/project-three",
+        liveSite: "https://example.com/project-three"
+    },
+    {
+        id: 4,
+        title: "Project Four",
+        subtitle: "A fourth project",
+        image: "https://placehold.co/500x400/000000/FFF",
+        tech: ["Vue", "Vuex", "MongoDB"],
+        link: "/projects/project-four",
+        liveSite: "https://example.com/project-four"
+    },
+    {
+        id: 5,
+        title: "Project Five",
+        subtitle: "A fifth project",
+        image: "https://placehold.co/500x400/000000/FFF",
+        tech: ["Angular", "NgRx", "Firebase"],
+        link: "/projects/project-five",
+        liveSite: "https://example.com/project-five"
     }
+
     // Add more projects as needed
 ];
