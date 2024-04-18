0dc36fff24a25c2ecfc1dd614f5e6eb9270a3c55
Parth <parth.shiralkar@gmail.com>
2024-04-18 00:44:15 -0700

    Addtest project data

---
+++ src/_data/projects.js
@@ -0,0 +1,22 @@
+// src/_data/projects.js
+module.exports = [
+    {
+        id: 1,
+        title: "Project One",
+        subtitle: "An amazing project",
+        image: "https://placehold.co/600x400/000000/FFF",
+        tech: ["HTML", "CSS", "JavaScript"],
+        link: "/projects/project-one",
+        liveSite: "https://example.com/project-one"
+    },
+    {
+        id: 2,
+        title: "Project Two",
+        subtitle: "Another remarkable project",
+        image: "https://placehold.co/600x400/000000/FFF",
+        tech: ["Node.js", "Express", "MongoDB"],
+        link: "/projects/project-two",
+        liveSite: "https://example.com/project-two"
+    }
+    // Add more projects as needed
+];
