5d2d1b1f989b6f3d9f9a470e9604ca5fa8e63782
Parth <parth.shiralkar@gmail.com>
2024-04-18 00:45:43 -0700

    Add markdown dep

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const markdownIt = require("markdown-it");
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('css')
     eleventyConfig.addPassthroughCopy('admin')
@@ -5,6 +7,12 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('src/js')
     eleventyConfig.addPassthroughCopy('src/lib')
     eleventyConfig.addPassthroughCopy('src/fontfiles')
+
+    // Add a Markdown filter using markdown-it
+    eleventyConfig.addNunjucksFilter("markdownify", function(value) {
+        let markdown = markdownIt();
+        return markdown.render(value);
+    });
 
     return {
         passthroughFileCopy: true,
