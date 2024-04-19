61cd80768b8f9026bf430ee83f8b4b5449fd5211
Parth <parth.shiralkar@gmail.com>
2024-04-19 11:30:37 -0700

    WIP carousel implementation disabled

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -2,12 +2,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     new Glider(document.querySelector('.glider'), {
         slidesToShow: 1,
-        dots: '.dots',
+        dots: '#gliderDots',
         draggable: true,
         arrows: {
             prev: '.glider-prev',
             next: '.glider-next'
         },
+        scrollLock: true, // Lock the scroll to a slide perfectly.
         responsive: [
             {
                 // screens greater than >= 775px
@@ -23,10 +24,10 @@ document.addEventListener('DOMContentLoaded', function () {
                 // screens greater than >= 1024px
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 'auto',
-                    slidesToScroll: 1,
-                    itemWidth: 'auto',
-                    duration: 1
+                    slidesToShow: 3,
+                    slidesToScroll: 'auto',
+                    itemWidth: 150,
+                    duration: 0.25
                 }
             }
         ]
