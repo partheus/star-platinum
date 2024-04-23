7e632b8a06f628f97f9c6fe5e24db1dd8931e09f
Parth <parth.shiralkar@gmail.com>
2024-04-23 01:49:56 -0700

    Make provision for error page

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
 const markdownIt = require("markdown-it");
+const fs = require("fs");
+const NOT_FOUND_PATH = "_site/404.html";
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy('css')
@@ -13,6 +15,29 @@ module.exports = function (eleventyConfig) {
         let markdown = markdownIt();
         return markdown.render(value);
     });
+
+    // add browsersync config for 404 page fallback and callback
+    eleventyConfig.setBrowserSyncConfig({
+        callbacks: {
+            ready: function (err, bs) {
+                bs.addMiddleware("*", (req, res) => {
+                    if (!fs.existsSync(NOT_FOUND_PATH)) {
+                        throw new Error(
+                            `Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`
+                        );
+                    }
+
+                    const content_404 = fs.readFileSync(NOT_FOUND_PATH);
+                    // Add 404 http status code in request header.
+                    res.writeHead(404, {"Content-Type": "text/html; charset=UTF-8"});
+                    // Provides the 404 content without redirect.
+                    res.write(content_404);
+                    res.end();
+                });
+            },
+        },
+    });
+
 
     return {
         passthroughFileCopy: true,
