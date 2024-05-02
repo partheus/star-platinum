e4676e56e7e0e6904298d799b6770ca78d3683d2
Parth <parth.shiralkar@gmail.com>
2024-05-02 00:54:26 -0700

    Add project data

--- src/_data/projects.js
+++ src/_data/projects.js
@@ -9,16 +9,18 @@ module.exports = [
         link: "/projects/project-one",
         liveSite: "https://cana.com",
         year: "2021",
-        description: "A 3D printer for drinks.",
+        description: "Developed the navbar and a few modules for the site, helped set up Shopify for the backend.",
         typography: "Degular + Roboto Mono",
         colors: ["#003e4e", "#fff", "#6dcada", "#f20044", "#d3e900"],
         designDetails: "Clean and minimalistic design",
-        awards: ["Patrick Stewart Approves of it"]
+        awards: ["Patrick Stewart Approves of it"],
+        awardsLinks: ["https://www.businesswire.com/news/home/20230302005097/en/Sir-Patrick-Stewart-Joins-Cana-as-%E2%80%9CMission-Ambassador%E2%80%9D-to-Help-End-the-Global-Trash-Crisis"],
+        type: "website"
     },
     {
         id: 2,
         title: "The Life Story",
-        subtitle: "Another remarkable project",
+        subtitle: "A website, an ecosystem to address exploitation in the sex trade through an intersectional, systems-based, survivor-centered and trauma-informed lens.",
         image: "/img/webp/lifestory.webp",
         tech: ["Vue.js", "Webpack", "Ajax"],
         link: "/projects/project-two",
@@ -27,12 +29,13 @@ module.exports = [
         description: "The website \"The Life Story\" is a platform for sharing and documenting personal life stories. Users can create and publish their own life stories, allowing others to read and engage with them.",
         typography: "FreightSans + FreightSans Pro",
         colors: ["#FFFFFF", "#2E437D", "#E3E8EE", "#FF685B", "#D4DEED"],
-        designDetails: "Bold and vibrant design"
+        designDetails: "Bold and vibrant design",
+        type: "website"
     },
     {
         id: 3,
         title: "UHG",
-        subtitle: "A third project",
+        subtitle: "Massive insurance company selling insurance products under UnitedHealthcare, and health care services and care delivery aided by technology and data under Optum.",
         image: "/img/webp/uhg.webp",
         tech: ["Nunjucks", "Gulp", "DDEV"],
         link: "/projects/project-three",
@@ -41,7 +44,8 @@ module.exports = [
         description: "The UHG website is used as a platform for UnitedHealth Group, providing information about the company, its services, and its initiatives. It serves as a hub for users to access resources, explore career opportunities, and stay updated on the latest news and developments within the organization.",
         typography: "Optum Sans + Graphik",
         colors: ["#002677", "#0066F5", "#CCE0FC", "#FFFFFF", "#DA5AB6"],
-        designDetails: "Modern and sleek design"
+        designDetails: "Modern and sleek design",
+        type: "website"
     },
     {
         id: 4,
@@ -50,13 +54,15 @@ module.exports = [
         image: "/img/webp/disney.webp",
         tech: ["Wordpress", "SCSS", "COMPOSER"],
         link: "/projects/project-four",
-        liveSite: "https://example.com/project-four",
+        liveSite: "https://www.thechoosekindnessproject.org/",
         year: "2023",
         description: "This is a description of the project. Dolor sit amet, consectetur adipiscing elit. Nullam nec purus nec nunc ultricies aliquam. Nullam nec purus nec nunc ultricies aliquam.",
         typography: "Tenon + Work Sans",
         colors: ["#ffc300", "#007B5A", "#fbf4f2", "#2e50fF", "#ff376b"],
         designDetails: "Elegant and sophisticated design",
-        awards: ["Webby Award for Best User Interface"]
+        awards: ["Webby Nominee for Best User Interface", "Anthem Finalist for Best Website"],
+        awardsLinks: [""],
+        type: "website"
     },
     {
         id: 5,
@@ -65,12 +71,13 @@ module.exports = [
         image: "/img/webp/nike.webp",
         tech: ["React", "DDEV", "SASS", "Chokidar"],
         link: "/projects/project-five",
-        liveSite: "https://example.com/project-five",
+        liveSite: "https://nikesb.com",
         year: "2023 - 2024",
         description: "Nike SB is a website dedicated to showcasing and selling Nike's skateboarding products. It offers a range of skateboarding shoes, apparel, and accessories designed for skateboarders. The website also provides information about upcoming releases, events, and collaborations in the skateboarding community.",
         typography: "Bookmania + Roboto Mono + Gothic FB Wide",
         colors: ["#ff6123", "#79c06b", "#70c8fe", "#000000", "#f4e372"],
-        designDetails: "Dynamic and interactive design"
+        designDetails: "Dynamic and interactive design",
+        type: "website"
     },
     {
         id: 6,
@@ -79,12 +86,13 @@ module.exports = [
         image: "/img/webp/rallyhealth.webp",
         tech: ["Craft", "Docker", "Jenkins"],
         link: "/projects/project-six",
-        liveSite: "https://example.com/project-six",
+        liveSite: "https://rallyheath.com",
         year: "2021 - 2023",
         description: "Rally Health is a website that provides resources and tools for individuals to improve their overall health and well-being. It offers various services such as personalized health assessments, goal tracking, and access to a network of healthcare providers. The website aims to empower users to take control of their health and make positive lifestyle changes.",
         typography: "Helvetica Nue + Adelle",
         colors: ["#F26C32", "#48BFB0", "#F5885C", "#239F8E", "#4d4d4d"],
-        designDetails: "Sleek and professional design"
+        designDetails: "Sleek and professional design",
+        type: "website"
     },
     {
         id: 7,
@@ -93,67 +101,57 @@ module.exports = [
         image: "/img/webp/malarkey.webp",
         tech: ["composer", "webpack", "babel"],
         link: "/projects/project-seven",
-        liveSite: "https://example.com/project-seven",
+        liveSite: "https://www.malarkeyroofing.com/",
         year: "2023",
         description: "Malarkey Roofing is a website that provides information and services related to roofing. It offers products, resources, and solutions for residential and commercial roofing needs.",
         typography: "Geogrotesque",
         colors: ["#F8F8F8", "#D28122", "#24C26C", "#000", "#efefef"],
-        designDetails: "Minimalistic and clean design"
+        designDetails: "Minimalistic and clean design",
+        type: "website"
     },
     {
         id: 8,
-        title: "Adobe Procurement",
-        subtitle: "An eighth project",
-        image: "/img/webp/adobe.webp",
-        tech: ["vue", "spectrumCSS", "babel"],
-        link: "/projects/project-eight",
-        liveSite: "https://example.com/project-eight",
-        year: "2023",
-        description: "The Adobe Procurement website provides information and resources for procurement processes related to Adobe products and services. It offers guidance on purchasing Adobe solutions, managing licenses, and accessing support for procurement professionals.",
-        typography: "Cursive",
-        colors: ["#0265DC", "#e3342f", "#fbc447", "#4dc0b5", "#9561e2"],
-        designDetails: "Creative and unique design"
-    },
-    {
-        id: 9,
         title: "UHG Sustainability",
         subtitle: "A ninth project",
         image: "/img/webp/uhg-sus.webp",
         tech: ["php", "grunt", "DDEV"],
         link: "/projects/project-nine",
-        liveSite: "https://example.com/project-nine",
+        liveSite: "https://sustainability.uhg.com",
         year: "2022 - 2023",
         description: "The website UnitedHealth Group Sustainability provides information about the sustainability initiatives and efforts of UnitedHealth Group. It showcases the company's commitment to environmental stewardship, social responsibility, and community engagement. The website highlights various sustainability programs and initiatives undertaken by UnitedHealth Group to create a positive impact on the environment and society.",
         typography: "Optum Sans + Graphik",
         colors: ["#002677", "#0066F5", "#CCE0FC", "#FFFFFF", "#DA5AB6"],
-        designDetails: "User-friendly and intuitive design"
+        designDetails: "User-friendly and intuitive design",
+        type: "website"
     },
     {
-        id: 10,
+        id: 9,
         title: "Disney CSR",
         subtitle: "A ninth project",
         image: "/img/webp/disney-csr.webp",
         tech: ["wordpress", "lottie", "docker"],
         link: "/projects/project-ten",
-        liveSite: "https://example.com/project-nine",
+        liveSite: "https://impact.disney.com/",
         year: "2023",
         description: "The CSR (Corporate Social Responsibility) page of Disney provides information about Disney's initiatives and efforts towards social and environmental responsibility. It showcases the company's commitment to making a positive impact on the world through various programs and partnerships.",
         typography: "Museo Sans + Inspire TWDC",
         colors: ["#fff5f4", "#081351", "#ff6621", "#00d99e", "#e2b2ff"],
-        designDetails: "User-friendly and intuitive design"
+        designDetails: "User-friendly and intuitive design",
+        type: "website"
     },
     {
-        id: 11,
+        id: 10,
         title: "Listening Sessions",
         subtitle: "A ninth project",
         image: "/img/webp/calltolisten.webp",
         tech: ["react", "gatsby", "scss"],
         link: "/projects/project-eleven",
-        liveSite: "https://example.com/project-nine",
+        liveSite: "https://acalltolisten.org",
         year: "2021",
         description: "The website acalltolisten.org is used for hosting listening sessions and as a platform for a call to listen initiative.",
         typography: "FreightSans + FreightSans Pro",
         colors: ["#1c1c1c", "#41d2af", "#205f86", "#206f53", "#5c80ee"],
+        type: "website"
     }
 ];
 
