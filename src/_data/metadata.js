d43d4802981277d6d3231574654456e992475886
Parth <parth.shiralkar@gmail.com>
2024-05-03 01:12:30 -0700

    Add metadata stuff

---
+++ src/_data/metadata.js
@@ -0,0 +1,10 @@
+module.exports = {
+    title: "Parth Shiralkar - A Portfolio",
+    description: "A portfolio of Parth Shiralkar",
+    keywords: "portfolio, Parth Shiralkar, Parth, Shiralkar, Parth Shiralkar portfolio, personal website, personal portfolio, web developer, web designer, front-end developer, front-end engineer, full-stack developer, full-stack engineer, software engineer, software developer, web developer portfolio, web designer portfolio, front-end developer portfolio, front-end engineer portfolio, full-stack developer portfolio, full-stack engineer portfolio, software engineer portfolio, software developer portfolio, web development, web design, front-end development, front-end engineering, full-stack development, full-stack engineering, software development, web development portfolio, web design portfolio, front-end development portfolio, front-end engineering portfolio, full-stack development portfolio, full-stack engineering portfolio, software development portfolio",
+    author: "Parth Shiralkar",
+    ogImage: "https://parth.ninja/assets/social-image.jpg", // Path to your default Open Graph image
+    twitterImage: "https://parth.ninja/assets/social-image.jpg", // Path to your default Twitter Card image
+    baseUrl: "https://work.parth.ninja", // Base URL of your site
+    robotsContent: "index, follow" // Control indexing behavior
+};
