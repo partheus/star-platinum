bcd05b3917dbfd586804d76dfc9f664de15938a7
Parth <parth.shiralkar@gmail.com>
2024-09-30 22:16:08 +0530

    Add scaffolding for print designs

---
+++ src/_data/printDesigns.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        id: 1,
+        image: "/img/print-designs/1.webp",
+        title: "Book Cover Design",
+    },
+    {
+        id: 2,
+        image: "/img/print-designs/2.webp",
+        title: "Magazine Layout",
+    }
+];
