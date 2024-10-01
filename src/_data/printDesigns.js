570eec5695925a3c8687e4b26aeb659fda446421
Parth <parth.shiralkar@gmail.com>
2024-10-01 21:34:21 +0530

    Create new data source

--- src/_data/printDesigns.js
+++ src/_data/printDesigns.js
@@ -1,12 +1,41 @@
-module.exports = [
-    {
-        id: 1,
-        image: "/img/print-designs/1.webp",
-        title: "Book Cover Design",
-    },
-    {
-        id: 2,
-        image: "/img/print-designs/2.webp",
-        title: "Magazine Layout",
-    }
-];
+module.exports = {
+    bookCovers: [
+        {
+            id: 1,
+            image: "/img/print-designs/1.webp",
+            title: "The Mysterious Island Cover",
+        },
+        {
+            id: 2,
+            image: "/img/print-designs/2.webp",
+            title: "Modern Sci-Fi Cover",
+        },
+        {
+            id: 3,
+            image: "/img/print-designs/3.webp",
+            title: "Fantasy Adventure Cover",
+        },
+        {
+            id: 4,
+            image: "/img/print-designs/4.webp",
+            title: "Romantic Novel Cover",
+        },
+    ],
+    posters: [
+        {
+            id: 1,
+            image: "/img/print-designs/phil1.webp",
+            title: "Movie Premiere Poster",
+        },
+        {
+            id: 2,
+            image: "/img/print-designs/phil2.webp",
+            title: "Concert Tour Poster",
+        },
+        {
+            id: 3,
+            image: "/img/print-designs/JAWNZ.webp",
+            title: "Art Exhibition Poster",
+        }
+    ],
+};
