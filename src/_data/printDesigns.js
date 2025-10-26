b9fc11aef6c9c6ce7972cd4f9a36f18b25395d1a
Parth <parth.shiralkar@gmail.com>
2025-10-26 16:25:59 +0530

    Update book covers and names

--- src/_data/printDesigns.js
+++ src/_data/printDesigns.js
@@ -5,64 +5,52 @@ module.exports = {
             title: "Short Stories eBook Cover",
         },
         {
-            id: 1,
-            image: "/img/print-designs/1.webp",
-            title: "Short Stories eBook Cover",
-        },
-        {
-            id: 2,
-            image: "/img/print-designs/2.webp",
+            image: "/img/print-designs/damage.webp",
             title: "Thriller eBook Cover",
         },
         {
-            id: 3,
-            image: "/img/print-designs/3.webp",
+            image: "/img/print-designs/caper.webp",
             title: "Short Story eBook Cover",
         },
         {
-            id: 4,
-            image: "/img/print-designs/4.webp",
+            image: "/img/print-designs/objects.webp",
+            title: "Short Stories eBook Cover",
+        },
+        {
+            image: "/img/print-designs/bug.webp",
             title: "Short Story eBook Cover",
         },
+        // {
+        //     image: "/img/print-designs/rare1.webp",
+        //     title: "Rare Objects Old Book Cover",
+        // },
+        // {
+        //     image: "/img/print-designs/rare2.webp",
+        //     title: "Rare Objects Old Book Cover 2",
+        // },
         {
-            id: 5,
-            image: "/img/print-designs/rare1.webp",
-            title: "Rare Objects Old Book Cover",
-        },
-        {
-            id: 6,
-            image: "/img/print-designs/rare2.webp",
-            title: "Rare Objects Old Book Cover 2",
-        },
-        {
-            id: 7,
             image: "/img/print-designs/spaces.webp",
             title: "Collection of Passages eBook Cover",
         }
     ],
     posters: [
         {
-            id: 1,
             image: "/img/print-designs/phil1.webp",
             title: "Philosophy Club Poster",
         },
         {
-            id: 2,
             image: "/img/print-designs/phil2.webp",
             title: "Philosophy Club Poster 2",
         },
         {
-            id: 3,
             image: "/img/print-designs/JAWNZ.webp",
             title: "JAWNZ Sneaker x Movie Poster",
         },
         {
-            id: 4,
             image: "/img/print-designs/swissevent.webp",
             title: "Swiss Event Poster",
         },
         {
-            id: 5,
             image: "/img/print-designs/pugspotify.webp",
             title: "Spotify Mockup Poster",
         }
